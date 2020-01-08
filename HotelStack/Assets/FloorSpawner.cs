65121b7a1f4766a22c8326134740b40762d984ea
Greg-Rus <grzegorz.rus@gmail.com>
2020-01-08 21:41:09 +0100

    Floor stacking mechanic

--- HotelStack/Assets/FloorSpawner.cs
+++ HotelStack/Assets/FloorSpawner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,19 +7,17 @@ public class FloorSpawner : MonoBehaviour
 {
     public FloorErector FloorPrefab;
     public float StartingSideLength;
-    //public float CurrentWidth;
-    //public float CurrentDepth;
     public Vector2 FloorDimensions;
     public float MovementSpeed;
     public FloorErector CurrentFloor;
     public int HotelHeight = 0;
     public float FloorSpawnOffset;
-    public Vector3 CurrentAxis = Vector3.left;
     public Vector3 FloorOrigin = Vector3.zero;
     public float SnapZoneSize;
+    public Vector3 Destination;
 
     public Vector3 CurrentDirection = Vector3.left;
-    // Start is called before the first frame update
+
     void Start()
     {
         FloorDimensions = new Vector2(StartingSideLength, StartingSideLength);
@@ -29,10 +28,10 @@ public class FloorSpawner : MonoBehaviour
     {
         CurrentFloor = Instantiate(FloorPrefab, Vector3.zero, Quaternion.identity);
         CurrentFloor.BuildFloor(FloorDimensions.x, FloorDimensions.y);
-        CurrentFloor.transform.position = FloorOrigin + (Vector3.right * FloorSpawnOffset);
+        CurrentFloor.transform.position = FloorOrigin + (CurrentDirection * FloorSpawnOffset * -1);
+        Destination = FloorOrigin + CurrentDirection * FloorSpawnOffset;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -42,7 +41,8 @@ public class FloorSpawner : MonoBehaviour
         else
         {
             CurrentFloor.transform.Translate(CurrentDirection * Time.smoothDeltaTime * MovementSpeed);
-            if (Mathf.Abs(CurrentFloor.transform.position.x) >= FloorOrigin.x + FloorSpawnOffset)
+            var distanceFromOrigin = (FloorOrigin - CurrentFloor.transform.position).magnitude;
+            if (distanceFromOrigin >= FloorSpawnOffset)
             {
                 CurrentDirection *= -1;
             }
@@ -52,10 +52,10 @@ public class FloorSpawner : MonoBehaviour
     private void OnFloorDrop()
     {
         var restingPos = CurrentFloor.transform.position;
-        var overhang = FloorOrigin.x - restingPos.x;
-        if (Mathf.Abs(overhang) < SnapZoneSize) SnapToOrigin();
-        else if(overhang < 0) SnipRight(overhang);
-        else SnipLeft(overhang);
+        var overhang = restingPos - FloorOrigin;
+
+        if (overhang.magnitude < SnapZoneSize) SnapToOrigin();
+        else SnipFloor(overhang);
 
         HotelHeight++;
         FloorOrigin += Vector3.up * 2; 
@@ -63,26 +63,20 @@ public class FloorSpawner : MonoBehaviour
         SpawnNewFloor();
     }
 
+    private void SnipFloor(Vector3 overhang)
+    {
+        if(overhang.x > 0) FloorOrigin = CurrentFloor.transform.position;
+        FloorDimensions.x -= Math.Abs(overhang.x);
+    }
+
     private void SpawnNewFloor()
     {
-        CurrentDirection = Vector3.left;
+        CurrentDirection = CurrentDirection == Vector3.left ? Vector3.back : Vector3.left;
         SpawnFloor();
     }
 
     private void SnapToOrigin()
     {
         CurrentFloor.transform.position = FloorOrigin;
-    }
-
-    private void SnipLeft(float overhang)
-    {
-        FloorOrigin = CurrentFloor.transform.position;
-        FloorDimensions.x -= Mathf.Abs(overhang);
-    }
-
-    private void SnipRight(float overhang)
-    {
-        FloorDimensions.x -= Mathf.Abs(overhang);
-        //Just cut the floor. Origin remains.
     }
 }
