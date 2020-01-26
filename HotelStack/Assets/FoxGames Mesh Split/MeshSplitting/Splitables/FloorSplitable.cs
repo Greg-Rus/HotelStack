64240e7d05e9e9dad02d7d1336216944087b2975
Greg-Rus <grzegorz.rus@gmail.com>
2020-01-26 14:02:17 +0100

    Correct colliders for detached section

--- HotelStack/Assets/FoxGames Mesh Split/MeshSplitting/Splitables/FloorSplitable.cs
+++ HotelStack/Assets/FoxGames Mesh Split/MeshSplitting/Splitables/FloorSplitable.cs
@@ -14,14 +14,46 @@ namespace MeshSplitting.Splitables
         protected override void PostProcessBottomObject(GameObject go)
         {
             var physicsFloorHalf = go.GetComponent<Floor>();
-            physicsFloorHalf.gameObject.AddComponent<BoxCollider>();
+            //physicsFloorHalf.gameObject.AddComponent<BoxCollider>();
             physicsFloorHalf.Rigidbody.isKinematic = false;
 
-            
-
-            physicsFloorHalf.Rigidbody.AddForce(SplitPlane.Normal * -SplitForce,ForceMode.Impulse);
+            physicsFloorHalf.Rigidbody.AddForce(SplitPlane.Normal * -SplitForce, ForceMode.Impulse);
             var torqueVector = Vector3.Cross(SplitPlane.Normal, Vector3.up);
             physicsFloorHalf.Rigidbody.AddRelativeTorque(torqueVector * SplitTorque);
+            AddColliders(go, SplitPlane.Normal);
         }
+
+        private void AddColliders(GameObject go, Vector3 splitNormal)
+        {
+            //AddColliderAtSide(go, Vector3.forward);
+            //AddColliderAtSide(go, Vector3.back);
+            //AddColliderAtSide(go, Vector3.left);
+            //AddColliderAtSide(go, Vector3.right);
+            //AddColliderAtSide(go, Vector3.down);
+
+            AddColliderAtSide(go, splitNormal.normalized * -1);
+            AddColliderAtSide(go, Vector3.Cross(splitNormal, Vector3.up));
+            AddColliderAtSide(go, Vector3.Cross(splitNormal, Vector3.down));
+            AddColliderAtSide(go, Vector3.down);
+        }
+
+        private void AddColliderAtSide(GameObject go, Vector3 side)
+        {
+            var collider = go.AddComponent<BoxCollider>();
+            var direction = side.ComponentSum();
+            var inverted = side.Inverted();
+            var value = direction < 0 ? 0 : Vector3.Scale(collider.center, side).ComponentSum() * 2;
+
+            var center = Vector3.Scale(collider.center, inverted);
+            center += side * value;
+            collider.center = center;
+
+            collider.size = Vector3.Scale(collider.size, inverted);
+            collider.size += side * 0.1f;
+
+            collider.center = collider.center + (side * (0.1f * -0.5f));
+        }
+
+
     }
 }
---
+++ HotelStack/Assets/Scripts/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public static class Helpers
+{
+    private const float _tolerance = 0.1f;
+    public static float ComponentSum(this Vector3 vector)
+    {
+        return vector.x + vector.y + vector.z;
+    }
+
+    public static Vector3 Inverted(this Vector3 vector)
+    {
+        return new Vector3(
+            Math.Abs(vector.x) > _tolerance ? 0 : 1,
+            Math.Abs(vector.y) > _tolerance ? 0 : 1, 
+            Math.Abs(vector.z) > _tolerance ? 0 : 1
+            );
+    }
+}
